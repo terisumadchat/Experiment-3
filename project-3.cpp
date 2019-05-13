868737b77341ac9326d4d8b7d0d7050a5206376d
terisumadchat <47727607+terisumadchat@users.noreply.github.com>
2019-05-13 23:29:04 +0800

    Update project-3.cpp

--- project-3.cpp
+++ project-3.cpp
@@ -16,7 +16,7 @@ for (x=0; x<size; x++)
 {
 	cin>>arr[x];
 }
-	cout<<"Sorting.... (selection sort)"<<endl;
+	cout<<"////////////////////////////////////////////////////"<<endl;
 
 for(x=0; x<size; x++)
 {
@@ -30,12 +30,12 @@ temp=arr[x];
 arr[x]=arr[y];
 arr[y]=temp;
 }}}}
-cout<<"result: ";
+cout<<"result: { ";
 
 for (x=0;x<size;x++){
 cout<<arr[x]<<",";
 }
-cout<<"}"<<endl;
+cout<<" }"<<endl;
 cout<<"The size of given array is: "<< size;
 getch();
 return 0;
